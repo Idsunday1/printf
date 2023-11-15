6841c889448ba5dbc412693170918b1956a8b901
Idsunday1 <idorenyinsunday94@gmail.com>
2023-11-14 16:55:45 -0800

    updated printf

--- 0-all_tasks_manager.c
+++ 0-all_tasks_manager.c
@@ -1,6 +1,30 @@
 #include "main.h"
 
 /**
+ * string_print - a function that prints a string
+ *
+ * @args_ls: va_list containing the actual string to print
+ *
+ * Return: no. of chars printed
+ */
+int string_print(va_list args_ls)
+{
+        int record = 0;
+        char *strn_pt = va_arg(arg_ls, char *);
+
+        if (strn_pt == NULL)
+        {
+                strn_pt = "(null)";
+        }
+        while (*strn_pt)
+        {
+                record += _putchar(*strn_pt);
+                strn_pt++;
+	}
+	return (record);
+}
+
+/**
  * chars_print - a function that prints a character
  *
  * @args_ls: va_list containing the actual character to print
@@ -13,4 +37,17 @@ int chars_print(va_list arg_ls)
 
         record = _putchar(charsit);
         return (record);
+}
+
+ /**
+ * percent_print - a function that prints a percent sign '%'
+ *
+ * @args_ls: va_list containing the % to print
+ *
+ * Return: no. of chars printed
+ */
+int percent_print(va_list arg_ls)
+{
+        (void)args_ls;
+        return (_putchar('%'));
 }
--- main.h
+++ main.h
@@ -9,6 +9,7 @@
 #include <string.h>
 
 int chars_print(va_list arg_ls);
+int string_print(va_list args_ls);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*ma_selector(char strn))(va_list args_ls);
