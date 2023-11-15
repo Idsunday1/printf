8d8afd5643e5446859b092ea7d8851abee7db99c
Idsunday1 <idorenyinsunday94@gmail.com>
2023-11-14 17:08:54 -0800

    updated main.h

--- main.h
+++ main.h
@@ -8,8 +8,11 @@
 #include <unistd.h>
 #include <string.h>
 
-int chars_print(va_list arg_ls);
 int string_print(va_list args_ls);
+int chars_print(va_list arg_ls);
+int percent_print(va_list arg_ls);
+int d_print(va_list args_ls);
+int i_print(va_list args_ls);
 int _putchar(char c);
 int _printf(const char *format, ...);
 int (*ma_selector(char strn))(va_list args_ls);
